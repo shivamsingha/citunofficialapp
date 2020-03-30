40aeeeca184ef2f95cbd8658ae4a19ae29b49376
shivam <shivamgit@pm.me>
2020-03-30 20:12:59 +0530

    Fix result file path
    Signed-off-by: shivam <shivamgit@pm.me>

--- server/utils/compare.js
+++ server/utils/compare.js
@@ -1,17 +1,22 @@
 const fs = require('fs').promises;
 const scraper = require('./scraper');
 
+const filePath = `${process.env.RESULT_PATH || process.cwd()}/result.json`;
+
 async function compare() {
-  let newResult, oldResult;
+  let newResult,
+    oldResult,
+    flag = false;
   try {
     newResult = await scraper();
     try {
-      oldResult = await fs.readFile('result.json', { flag: 'a+' });
+      oldResult = await fs.readFile(filePath, { flag: 'a+' });
       const newResultString = JSON.stringify(newResult);
       if (oldResult.toString() != newResultString) {
+        flag = true;
         console.log('result changed. writing to json file');
         try {
-          await fs.writeFile('../result.json', newResultString);
+          await fs.writeFile(filePath, newResultString);
         } catch (e) {
           console('fs.writeFile error');
           console.log(e);
@@ -27,6 +32,7 @@ async function compare() {
     console.log('Scraper Error');
     console.log(e);
   }
+  return flag;
 }
 
-compare();
+module.exports = compare;
