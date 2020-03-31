677b0dcc333a5e55a9ecd679c24ba9ca8f96c991
shivam <shivamgit@pm.me>
2020-03-31 23:58:03 +0530

    Add FCM
    Signed-off-by: shivam <shivamgit@pm.me>

--- server/index.js
+++ server/index.js
@@ -1,5 +1,34 @@
 const compare = require('./utils/compare');
+const admin = require('firebase-admin');
+
+const serviceAccount = require(process.env.SERVICE_ACC_KEY ||
+  './serviceAccountKey.json');
+const topic = 'allDevices';
 
 compare().then((status) => {
-  if (status) console.log('pushed notification');
+  if (status.wasChanged) {
+    let message = {
+      notification: {
+        title: 'New Notice'
+      },
+      data: {
+        diff: JSON.stringify(status.diff)
+      },
+      topic: topic
+    };
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: 'https://citunofficial.firebaseio.com'
+    });
+
+    admin
+      .messaging()
+      .send(message)
+      .then((res) => {
+        console.log('pushed notification ', res);
+      })
+      .catch((e) => {
+        console.log('push ERROR ', e);
+      });
+  }
 });
