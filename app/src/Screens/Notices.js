e1162aa24f80870dc7f2f0eaeb0e44b67ce57639
shivam <shivamgit@pm.me>
2020-04-04 22:58:02 +0530

    Use redux data
    Signed-off-by: shivam <shivamgit@pm.me>

--- app/src/Screens/Notices.js
+++ app/src/Screens/Notices.js
@@ -6,7 +6,9 @@
  * @flow
  */
 
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchData } from '../redux/actions';
 import { SafeAreaView, StyleSheet, View, StatusBar } from 'react-native';
 import Animated from 'react-native-reanimated';
 import { ListItem } from '../Components';
@@ -14,81 +16,93 @@ import { relativeHeight } from '../utils';
 
 const { Value, interpolate, Extrapolate, set } = Animated;
 
-const App: () => React$Node = () => {
-  const [scrollY] = useState(new Value(0));
+class Notices extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollY: new Value(0)
+    };
+  }
 
-  const imageOpacity = () =>
-    interpolate(scrollY, {
+  componentDidMount() {
+    this.props.fetchData();
+  }
+
+  imageOpacity = () =>
+    interpolate(this.state.scrollY, {
       inputRange: [0, 150],
       outputRange: [1, 0],
       extrapolate: Extrapolate.CLAMP
     });
 
-  const imageSize = () =>
-    interpolate(scrollY, {
+  imageSize = () =>
+    interpolate(this.state.scrollY, {
       inputRange: [0, 150],
       outputRange: [1.0, 0.3],
       extrapolate: Extrapolate.CLAMP
     });
 
-  const imageTranslateY = () =>
-    interpolate(scrollY, {
+  imageTranslateY = () =>
+    interpolate(this.state.scrollY, {
       inputRange: [0, 150],
       outputRange: [0, -105],
       extrapolate: Extrapolate.CLAMP
     });
 
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Animated.Image
-          source={require('../../assets/Logo.png')}
-          resizeMode="contain"
-          style={[
-            styles.headerBackground,
-            {
-              opacity: imageOpacity(),
-              transform: [
-                { scale: imageSize() },
-                { translateY: imageTranslateY() }
-              ]
-            }
-          ]}
-        />
-        <Animated.ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}
-          scrollEventThrottle={1}
-          onScroll={Animated.event(
-            [
-              {
-                nativeEvent: { contentOffset: { y: y => set(scrollY, y) } }
-              }
-            ],
-            { useNativeDriver: true }
-          )}>
-          <View
+  render() {
+    const { fetchStatus, data } = this.props;
+    const { scrollY } = this.state;
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <Animated.Image
+            source={require('../../assets/Logo.png')}
+            resizeMode="contain"
             style={[
-              styles.body,
-              { transform: [{ translateY: relativeHeight(30) }] }
-            ]}>
-            {JSON.parse(JSON.stringify(require('../result.json'))).notices.map(
-              obj => (
-                <ListItem
-                  key={obj.link}
-                  link={obj.link}
-                  date={obj.date}
-                  title={obj.title}
-                />
-              )
-            )}
-          </View>
-        </Animated.ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+              styles.headerBackground,
+              {
+                opacity: this.imageOpacity(),
+                transform: [
+                  { scale: this.imageSize() },
+                  { translateY: this.imageTranslateY() }
+                ]
+              }
+            ]}
+          />
+          <Animated.ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}
+            scrollEventThrottle={1}
+            onScroll={Animated.event(
+              [
+                {
+                  nativeEvent: { contentOffset: { y: y => set(scrollY, y) } }
+                }
+              ],
+              { useNativeDriver: true }
+            )}>
+            <View
+              style={[
+                styles.body,
+                { transform: [{ translateY: relativeHeight(30) }] }
+              ]}>
+              {data &&
+                data.notices.map(obj => (
+                  <ListItem
+                    key={obj.link}
+                    link={obj.link}
+                    date={obj.date}
+                    title={obj.title}
+                  />
+                ))}
+            </View>
+          </Animated.ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   scrollView: {
@@ -108,4 +122,7 @@ const styles = StyleSheet.create({
   }
 });
 
-export default App;
+export default connect(
+  state => state.fetchedData,
+  { fetchData }
+)(Notices);
